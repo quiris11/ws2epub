43d3a219196bdba8a3ae60f4810252650bfb0d0c
Robert Błaut <listy@blaut.biz>
2014-07-27 20:45:57 +0200

    work in progress

--- __main__.py
+++ __main__.py
@@ -17,7 +17,9 @@ __author__ = u'Robert Błaut <listy@blaut.biz>'
 import argparse
 import sys
 import urllib2
-from lxml import html
+import re
+
+from lxml import etree
 
 
 parser = argparse.ArgumentParser()
@@ -28,10 +30,48 @@ args = parser.parse_args()
 
 
 def main():
-    content = urllib2.urlopen(args.url)
-    doc = html.parse(content)
-    print(html.tostring(doc))
+    cf = urllib2.urlopen(args.url)
+    # doc = html.parse(content)
+    content = cf.read()
+    with open("raw.txt", "w") as text_file:
+        text_file.write(content)
+    content = re.sub(r'(\s+[a-z0-9]+)=([\'|\"]{0})([a-z0-9]+)([\'|\"]{0})',
+                     r'\1="\3"',
+                     content)
+    with open("raw-changed.txt", "w") as text_file:
+        text_file.write(content)
+    # parser = etree.XMLParser(recover=True)
+    # doc = etree.parse(content, parser)
+    tree = etree.fromstring(content)
 
+    # print(content.read())
+
+    # alltexts = tree.xpath('//body//text()')
+    for s in tree.xpath('//script'):
+        s.getparent().remove(s)
+    # alltext = ' '.join(alltexts)
+    # print(alltext)
+    for s in tree.xpath('//div[@id="mw-navigation"]'):
+        s.getparent().remove(s)
+    for s in tree.xpath('//div[@id="catlinks"]'):
+        s.getparent().remove(s)
+    for s in tree.xpath('//div[@id="footer"]'):
+        s.getparent().remove(s)
+    for s in tree.xpath('//div[@id="Template_law"]'):
+        s.getparent().remove(s)
+    # for s in tree.xpath('//img[@alt="Przypis własny Wikiźródeł"]'):
+        # s.getparent().remove(s)
+    # for s in tree.xpath('//a[@href="/wiki/Pomoc:Przypisy"]'):
+        # s.getparent().remove(s)
+    for s in tree.xpath('//span/span[@class="PageNumber"]'):
+        s.getparent().remove(s)
+    with open("raw-changed.html", "w") as text_file:
+        text_file.write(etree.tostring(
+            tree,
+            pretty_print=True,
+            xml_declaration=True, encoding='utf-8', standalone=False
+        ))
+    # print(etree.tostring(tree))
     if len(sys.argv) == 1:
         print("* * *")
         print("* At least one of above optional arguments is required.")

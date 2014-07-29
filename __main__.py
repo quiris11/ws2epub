1b1b600ff398d30c6e32a6561654825e08520608
Robert Błaut <listy@blaut.biz>
2014-07-29 15:34:41 +0200

    Work in progress

--- __main__.py
+++ __main__.py
@@ -21,12 +21,18 @@ import re
 import os
 import zipfile
 import unicodedata
+import shutil
+from urllib import unquote
 from urllib import urlretrieve
 
 from lxml import etree
 SFENC = sys.getfilesystemencoding()
 DTD = ('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" '
        '"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">')
+OPFNS = {'opf': 'http://www.idpf.org/2007/opf'}
+XHTMLNS = {'xhtml': 'http://www.w3.org/1999/xhtml'}
+DCNS = {'dc': 'http://purl.org/dc/elements/1.1/'}
+NCXNS = {'ncx': 'http://www.daisy.org/z3986/2005/ncx/'}
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-V', '--version', action='version',
@@ -36,6 +42,17 @@ args = parser.parse_args()
 
 
 def pack_epub(output_filename, source_dir):
+    the_file = '.DS_Store'
+    try:
+        os.unlink(os.path.join(source_dir, the_file))
+    except:
+        pass
+    for root, dirs, files in os.walk(source_dir):
+        for d in dirs:
+            try:
+                os.unlink(os.path.join(root, d, the_file))
+            except:
+                pass
     with zipfile.ZipFile(output_filename, "w") as zip:
         zip.writestr("mimetype", "application/epub+zip")
     relroot = source_dir
@@ -70,47 +87,60 @@ def remove_node(node):
     parent.remove(node)
 
 
-def download_images(tree):
+def download_images(tree, btitle, bauthor):
     num = 1
+    parser = etree.XMLParser(remove_blank_text=True)
+    opftree = etree.parse(os.path.join('WSepub/OPS/content.opf'), parser)
+    ncxtree = etree.parse(os.path.join('WSepub/OPS/toc.ncx'), parser)
+    manifest = opftree.xpath('//opf:manifest', namespaces=OPFNS)[0]
+    if not isinstance(unquote(args.url), unicode):
+        url = unquote(args.url).decode('utf-8')
+    opftree.xpath('//dc:source', namespaces=DCNS)[0].text = url
+    opftree.xpath('//dc:identifier', namespaces=DCNS)[0].text = url
+    opftree.xpath('//dc:title', namespaces=DCNS)[0].text = btitle
+    opftree.xpath('//dc:creator', namespaces=DCNS)[0].text = bauthor
+    # print(etree.tostring(ncxtree.xpath('//ncx:docTitle/ncx:text', namespaces=NCXNS)[0]))
+    # sys.exit()
+    ncxtree.xpath(
+        '//ncx:meta', namespaces=NCXNS
+    )[0].attrib['content'] = url
+    ncxtree.xpath(
+        '//ncx:docTitle/ncx:text', namespaces=NCXNS
+    )[0].text = btitle
+    ncxtree.xpath(
+        '//ncx:docAuthor/ncx:text', namespaces=NCXNS
+    )[0].text = bauthor
     for i in tree.xpath('//img'):
         filext = os.path.splitext(i.get('src').split("/")[-1])[1]
-        urlretrieve('https:' + i.get('src'), os.path.join('WSepub',
-                                                          'Images',
-                                                          str(num) + filext))
-        i.attrib['src'] = '../Images/' + str(num) + filext
+        urlretrieve('https:' + i.get('src'), os.path.join(
+            'WSepub', 'OPS', 'Images', 'img_' + str(num) + filext
+        ))
+        i.attrib['src'] = '../Images/img_' + str(num) + filext
+        opfitem = etree.fromstring(
+            '<item id="img_%s" href="Images/img_%s%s" media-'
+            'type="image/jpeg"/>' % (num, num, filext))
+        manifest.append(opfitem)
         num += 1
-
-
-def create_link_css():
-    css = '.small { font-size: 94%; }\n' \
-        '.thumbcaption {text-align:center;font-size:90%;font-style:italic;}\n'\
-        '.tright { float: right; clear: right; margin: 0.5em 0 0.5em 1em; }\n'\
-        '.tleft { float: left; clear: left; margin: 0.5em 1em 0.5em 0; }\n' \
-        '.poem { display: table; margin: 0 auto; }\n' \
-        '.center { display: table; margin: 0 auto; }\n' \
-        '.center, .center * {text-align: center;}\n' \
-        'p { text-align: justify; margin: 0; text-indent: 1.2em; }\n' \
-        'h1, h2, h3, h4, h5, h6 {text-align: center;}\n'
-    # '.center { width : 100%; text-align: center; }\n' \
-    # '.center * { margin-left: auto; margin-right: auto; }\n' \
-    with open("WSepub/Styles/style.css", "w") as text_file:
-        text_file.write(css)
+    with open(os.path.join('WSepub/OPS/content.opf'), 'w') as f:
+        f.write(etree.tostring(opftree.getroot(), pretty_print=True,
+                standalone=False, xml_declaration=True, encoding='utf-8'))
+    with open(os.path.join('WSepub/OPS/toc.ncx'), 'w') as f:
+        f.write(etree.tostring(ncxtree.getroot(), pretty_print=True,
+                standalone=False, xml_declaration=True, encoding='utf-8'))
 
 
 def main():
-    if not os.path.isdir('WSepub'):
-        os.makedirs('WSepub')
-    if not os.path.isdir(os.path.join('WSepub', 'Text')):
-        os.makedirs(os.path.join('WSepub', 'Text'))
-    if not os.path.isdir(os.path.join('WSepub', 'Images')):
-        os.makedirs(os.path.join('WSepub', 'Images'))
-    if not os.path.isdir(os.path.join('WSepub', 'Styles')):
-        os.makedirs(os.path.join('WSepub', 'Styles'))
+    if os.path.exists('WSepub'):
+        shutil.rmtree('WSepub')
+    shutil.copytree(os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        'resources'), 'WSepub')
     cf = urllib2.urlopen(args.url)
     # doc = html.parse(content)
     content = cf.read()
     # with open("raw.txt", "w") as text_file:
     #     text_file.write(content)
+    content = content.replace('&#160;', ' ')
     content = re.sub(r'(\s+[a-z0-9]+)=([\'|\"]{0})([a-z0-9]+)([\'|\"]{0})',
                      r'\1="\3"',
                      content)
@@ -125,6 +155,13 @@ def main():
     # alltext = ' '.join(alltexts)
     # print(alltext)
     book = tree.xpath('//div[@id="mw-content-text"]')[0]
+    # bauthor = tree.xpath('//tr[td/text() = "Autor"]/td/a/text()')[0]
+    # tytul = 'Tytuł'.decode('utf-8')
+    # btitle = tree.xpath('//tr[td/text() = "' + tytul + '"]/td/a/text()')[0]
+    if tree.xpath('//table[@class="infobox"]/tr[2]/td[1]')[0].text == 'Autor':
+        bauthor = tree.xpath('//table[@class="infobox"]/tr[2]/td[2]/a')[0].text
+    if tree.xpath('//table[@class="infobox"]/tr[3]/td[1]')[0].text == u'Tytuł':
+        btitle = tree.xpath('//table[@class="infobox"]/tr[3]/td[2]/a')[0].text
     title = tree.xpath('//title')[0]
     del tree.xpath('//html')[0].attrib['lang']
     del tree.xpath('//html')[0].attrib['dir']
@@ -152,6 +189,8 @@ def main():
         remove_node(s)
     for s in tree.xpath('//noscript'):
         remove_node(s)
+    for s in tree.xpath('//div[@class="refsection"]'):
+        del s.attrib['style']
     for s in tree.xpath(
         '//div[@id="Template_law"]/div/div[@style="float: left;"]'
     ):
@@ -161,6 +200,7 @@ def main():
         app = s.getparent().getparent()
         remove_node(s.getparent())
         app.insert(0, at)
+
     for s in tree.xpath('//hr'):
         try:
             s.attrib['style'] = 'width: ' + s.attrib['width'] + 'px'
@@ -180,37 +220,15 @@ def main():
             del s.attrib['data-file-height']
         except:
             pass
-    # for s in tree.xpath('//script'):
-    #     remove_node(s)
-    # # for s in tree.xpath('//div[@id="content"]'):
-    #     # remove_node(s)
-    # for s in tree.xpath('//div[@id="mw-head-base"]'):
-    #     remove_node(s)
-    # for s in tree.xpath('//div[@id="mw-page-base"]'):
-    #     remove_node(s)
-    # for s in tree.xpath('//div[@id="mw-navigation"]'):
-    #     remove_node(s)
-    # for s in tree.xpath('//div[@id="catlinks"]'):
-    #     remove_node(s)
-    # for s in tree.xpath('//div[@id="footer"]'):
-    #     remove_node(s)
-    # for s in tree.xpath('//div[@id="Template_law"]'):
-    #     remove_node(s)
-    # for s in tree.xpath('//div[@class="printfooter"]'):
-    #     remove_node(s)
-    # for s in tree.xpath('//div[@class="visualClear"]'):
-    #     remove_node(s)
-    # # for s in tree.xpath('//img[@alt="Przypis własny Wikiźródeł"]'):
-    #     # remove_node(s)
-    download_images(tree)
-    create_link_css()
+
+    download_images(tree, btitle, bauthor)
     bs = etree.tostring(
         tree,
         pretty_print=True,
         xml_declaration=True, encoding='utf-8', standalone=False, doctype=DTD
     )
     bs = bs.replace('<span/>', '')
-    bs = bs.replace('href="#', 'href="text.html#')
+    bs = bs.replace('href="#', 'href="text.xhtml#')
     bs = bs.replace('href="/wiki', 'href="https://pl.wikisource.org/wiki')
     bs = bs.replace('src="//upload', 'src="https://upload')
     bs = bs.replace('<html>', '<html xmlns="http://www.w3.org/1999/xhtml">')
@@ -222,26 +240,68 @@ def main():
         r'<div \1>',
         bs
     )
+    bs = re.sub(
+        r'\[(\d+)\]</a></sup>',
+        r'\1</a></sup>',
+        bs
+    )
     bs = bs.replace('</p>', '</div>')
+    tree = etree.fromstring(bs)
+    # print(tree.xpath('//xhtml:div[@class="thumb tright"]', namespaces=XHTMLNS))
+    for i, s in enumerate(tree.xpath('///xhtml:div[@class="thumb tright"]', namespaces=XHTMLNS)):
+        # print(s.tail)
+        # print(etree.tostring(s.getparent()[s.getparent().index(s)-1]))
+        s.getparent()[s.getparent().index(s)-1].tail = s.getparent()[s.getparent().index(s)-1].tail + s.tail
+        s.tail = ''
+        # print(s.getparent()[s.getparent().index(s)+1].tag)
+        if s.getparent()[s.getparent().index(s)+1].tag == '{http://www.w3.org/1999/xhtml}br':
+            s.getparent().insert(s.getparent().index(s), etree.fromstring('<br/>'))
+            remove_node(s.getparent()[s.getparent().index(s)+1])
+        # s.getparent().insert(s.getparent().index(s)-1, s)
+        # a = tree.xpath('//xhtml:div[@class="thumb tright"][' + str(i+1) + ']/preceding-sibling::xhtml:div', namespaces=XHTMLNS)[-1]
+        # print(etree.tostring(a))
+        # print('#### ', i)
+    # for i, s in enumerate(tree.xpath('//div[@class="thumb tleft"]')):
+    #     a = tree.xpath('//div[@class="thumb tleft"][' + str(i+1) + ']/preceding-sibling::div')[-1]
+    #     print(etree.tostring(a))
+    # for s in tree.xpath('//xhtml:div[@class="thumbcaption"]/xhtml:div', namespaces=XHTMLNS):
+    #     print(etree.tostring(s))
+    #     print(s.text)
+    #     s.getparent().text = s.text
+    #     remove_node(s)
+    bs = etree.tostring(
+        tree,
+        pretty_print=True,
+        xml_declaration=True, encoding='utf-8', standalone=False, doctype=DTD
+    )
     bs = re.sub(
         r'\n<div class="para">'
-        '<span style="padding-left:18px; text-align:left;">.+</span>(.+)<br/>',
+        '<span style="padding-left:18px; text-align:left;">\s{1}</span>'
+        '(.+)<br/>',
         r'\n<div class="para"><p>\1</p>',
         bs
     )
     bs = re.sub(
-        r'\n<span style="padding-left:18px; text-align:left;">.+</span>'
+        r'\n<span style="padding-left:18px; text-align:left;">\s{1}</span>'
         '(.+)<br/>',
         r'\n<p>\1</p>',
+        bs
+    )
+    bs = re.sub(
+        r'\n<span style="padding-left:18px; text-align:left;">\s{1}</span>'
+        '(.+)\n\n(.+)<br/>',
+        r'\n<p>\1\2</p>',
         bs
     )
     bs = bs.replace(
         'id="Template_law" class="toccolours" style="border-width:1px 0 0 0"',
         'class="Template_law"'
     )
-    with open("WSepub/Text/text.html", "w") as text_file:
+    with open(os.path.join("WSepub/OPS/Text/text.xhtml"), "w") as text_file:
         text_file.write(bs)
     # print(etree.tostring(tree))
+
+    pack_epub(bauthor + ' - ' + btitle + '.epub', 'WSepub')
     if len(sys.argv) == 1:
         print("* * *")
         print("* At least one of above optional arguments is required.")
